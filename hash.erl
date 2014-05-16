f0058f0bbeb175b5425e93e724aa6c1422bedfed
Daniel Swensson <danielswensson@hotmail.com>
2014-05-16 11:24:16 +0200

    first

---
+++ hash.erl
@@ -0,0 +1,32 @@
+-module(hash).
+-compile(export_all).
+
+salt() -> "THIS IS THE SALT!".
+
+run(Dir) ->
+	{ok, Filenames} = file:list_dir(Dir),
+	hash(Filenames,Dir).
+
+
+hash([],_Dir) -> ok;
+hash([Name | T],Dir) ->
+	case filelib:is_dir(Dir ++ Name) of
+		true ->
+			
+			PathName = Dir ++ salt_and_hash(Name),
+			file:rename(Dir ++ Name, PathName),
+			{ok,Filenames} = file:list_dir(PathName),
+			ok = hash(Filenames,PathName ++ "/"),
+			hash(T,Dir);
+		_Else ->
+			PathName = Dir ++ salt_and_hash(Name),
+			file:rename(Dir ++ Name, PathName ++ ".java"),
+			hash(T,Dir)
+	end.
+
+salt_and_hash(Name) ->
+	RandomSalt = binary_to_list(crypto:rand_bytes(100)),
+	<<HashName:128>> = crypto:hash(md5,RandomSalt ++ salt() ++ Name),
+	integer_to_list(HashName). 
+
+	
